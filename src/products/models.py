b219459a6e1097c201129ebd00ce23e7dadf0a3a
petern160 <peternguyen160@gmail.com>
2019-06-25 15:58:07 -0700

    finished model products

--- src/products/models.py
+++ src/products/models.py
@@ -5,4 +5,7 @@ class Product(models.Model):
     title       = models.CharField(max_length=120) #max_length = required
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary     = models.TextField()
+    summary     = models.TextField(blank=False, null=False)
+    featured    = models.BooleanField() #null = true, default = true
+    #null has to deal with database can be empty or non empty
+    #blank has to do with field
