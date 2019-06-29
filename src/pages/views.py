bbb7396053c32571bc360651b94d31552f758979
petern160 <peternguyen160@gmail.com>
2019-06-28 17:00:23 -0700

    rendering from db

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,8 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>hello world<h1>") #string of html code
+def home_view(request, *args, **kwargs):
+    print(request.user)
+    print (args, kwargs)
+    # return HttpResponse("<h1>hello world<h1>") 
+    return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact<h1>") #
+def contact_view(request, *args, **kwargs):
+      return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+      
+    my_context = {
+          "my_text": "this is about us",
+          "my_number": 123,
+          "my_list": [123, 123, 321, 32134, "abc"],
+          "my_html": "<h1>hello world</h1>"
+    }
+    return render(request, "about.html", my_context)
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>social<h1>") #
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view, contact_view
+from pages.views import home_view, contact_view, about_view
+from products.views import product_detail_view
 # from pages.views import contact_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('contact/', contact_view, name='home'),
+    path('about/', about_view, name='home'),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
