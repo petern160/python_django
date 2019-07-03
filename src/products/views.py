042d83781c1c45307ec2657f0670c37f8fc871a6
petern160 <peternguyen160@gmail.com>
2019-07-02 17:20:48 -0700

    forms in django

--- src/products/views.py
+++ src/products/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
 
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 from .models import Product
+
+
 # Create your views here.
 
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
 
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
+
+
+# def product_create_view(request):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == "POST":
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+#     # Product.objec
+#     context = {
+#     }
+#     return render(request, "products/product_create.html", context)
+
+def product_create_view(request):
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
-        'form': form
+        "form": my_form
     }
     return render(request, "products/product_create.html", context)
 
